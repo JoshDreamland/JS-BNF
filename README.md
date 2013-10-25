80ca56e59121cd5e6b48e82b57e040cc9296f3ed
JoshDreamland <joshv@zoominternet.net>
2013-10-25 17:35:20 -0400

    Lab 2 submission; includes an interpreter for the "CORE" language.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 RUNNING
-###########
+######
 Please open index.html in Firefox or Chromium, or any HTML- and ECMA-compliant browser. This excludes ELinks. The version of Links available on the stdlinux cluster is a scriptless version of ELinks, and so is not suited to run this program. However, any compliant browser on any operating system is.
 
 In the event that you are accessing this file via secure shell, I recommend you do any of the following:
@@ -17,59 +17,49 @@ In the event that you are accessing this file via secure shell, I recommend you 
   4. Open index.htm in your favorite browser.
 
 3) Alternatively, you can run the files from my own student site. http://www.cse.ohio-state.edu/~venturaj/JS-BNF
+   I have updated this page with the latest code.
 
 Once you have the file open, you can run it on an arbitrary grammar entered in the box with the placeholder text, "BNF Here" (by default, it contains CORE's BNF). The grammar is run on the input in the box with the placeholder, "String to parse here" (by default, a simple CORE program).
 
 From there, a tree is built and added to the box labeled "Output goes here."
 
-Finally, the content of the third input box is read in. This box contains a JSON fragment describing the functions required to print the tokens from the tree as integers, as specified in the lab requirements.
-
+From there, you can press the "Run Interpreter" button to both pretty-print the code and invoke the interpreter on it. Pretty-printed code is written to the shadowed box on the left, while program output is written to the similar box on the right. Input will be requested in a popup window, or can be specified in bulk (as from a file) in the bottommost box.
 
 DESIGN
-###########
+######
 Rather than parse a specific grammar by creating functions to handle each rule, this project parses a BNF grammar, then generates a parse tree using the rules of that grammar. As a result, the project produces *concrete* parse trees. For the purpose of this lab, that is useful, as the individual items in the parse tree can be looked up against a map of each token to its number (more on this below).
 
 The code which generates the trees from BNF is reasonably well-documented. About a third of the lines in the main file are comments explaining individual functions, which are themselves commented reasonably well.
 
-Recursive operations on trees are performed by defining a JavaScript object which maps a function to each rule name. Rules which have not been mapped to a function are passed to the "generic" function. So, consider the supplied printtokens.json:
+Recursive operations on trees are performed by defining a JavaScript object which maps a function to each rule name. Rules which have not been mapped to a function are passed to the "generic" function.
 
-generic: function(ast, ops) {
-  res = "";
-  if (ast.type == "rule")
-    for (var i = 0; i < ast.ast.length; ++i)
-      res += (res.length? " ":"") + operate(ast.ast[i], ops);
-  else if (ast.type == "token")
-    res = ({ "program": 01, "begin":   02, ... ">=": 30 })[ast.name];
-  return res;
-},
-"<id>":  function(id, ops) { return 32; },
-"<int>": function(id, ops) { return 31; }
-
-The "generic" function tests if the given AST subtree actually is a rule. If it is, the function delegates to that rule, and the returned string of tokens is added to its own result.
-If the AST subtree passed to the generic function is instead a token, its index is looked up in a map of token names to token values, as given in the lab requiremenst.
-The rules "<id>" and "<int>" are special; since they contain a subtree of nonsense, they are replaced with fuctions which simply return 32 and 31, respectively, such that they appear to be a single token of the correct index.
-
+The file "interpreter.js" contains two such JavaScript objects, printer and interpreter. The printer is relatively simple; its functions map arrays of values to a basic default print function, "defprint." It is this function which is responsible for handling indentation, etc. The interpreter is more complicated; its methods do sanity checking to ensure that the grammar from which the tree was constructed actually resembles the expected grammar. See Caveats for more on this.
 
 FILES
-###########
+######
 README.md:         That's this file.
 index.htm:         The main page you can open in a web browser to interact with this parser. Also contains the code that prints the token IDs; you can view and edit that code in your browser.
 bnfparser.js:      The main source file; handles parsing and interpreting BNF rules to generate parse trees.
+interpreter.js:    The interpreter source file; defines the interpreter and printer objects.
 core.bnf:          The BNF grammar included in index.htm; a modified version of the grammar distributed by Dr. Soundarajan (see Remarks section).
-printtokens.json:  The JSON fragment included in index.htm. Contains functions which, when passed to bnf.operate() (bnfparser.js), print all the tokens in the parse tree.
+printtokens.json:  The JSON fragment that used to be included in index.htm. Contains functions which, when passed to bnf.operate() (bnfparser.js), print all the tokens in the parse tree.
 sample.core:       A simple CORE program sample.
 
 
 CAVEATS
-###########
+######
 Error checking is extremely approximate. Since this parser works for general BNF strings, it does backtracking. Therefore, it isn't possible to know whether the current production's expectation of the input is valid to report errors on. To work around this, all productions report their own errors, and a heuristic value is assigned to each error based on how many tokens were correctly read. The production which had consumed the most characters before ultimately failing is reported. In the event of a tie, multiple errors are reported, separated by an OR.
 
+Since this interpreter operates on a concrete AST, and does so in an interpreted language itself, running times can become quite slow (the amount of time spent interpreting is squared). You can minimize this by running in a browser such as Chrome/Chromium, which compiles JavaScript natively before running it.
+
+Because the AST is controlled by a grammar, and the grammar is completely separate from the interpreter, it is impossible to guarantee anything about the structure of the syntax tree which is passed to either printer or interpreter. To deal with this, the printer was written to be extremely dogmatic; code from a different grammar will print, but will look very ugly. The interpreter, conversely, is quite pedantic: it frequently checks that the tokens and whitespace in the concrete AST align with its own understanding of the structure, then evaluates them. It has some tolerance, but not nearly as much as the printer.
+
 ERRATA
-###########
+######
 It isn't entirely true, of course, that this parser is not affected by left recursion. Left recursion is dealt with by limiting the length of production paths which do not consume any characters. Thus, rules which require more than about eighty "useless" recursions will result in the failure of a perfectly valid string to parse. In the typical use case, this should never happen.
 
 REMARKS
-###########
+######
 The input must be syntactically valid CORE code. In the event that this is problematic, you could work around it by constructing a grammar which just lists all of the tokens and special symbols, instead, like this:
 
 <stuff>  ::= <etc> <stuff> | <etc>
@@ -89,19 +79,58 @@ The supplied CORE grammar, on that note, has been modified from the original gra
   4) I accidentally included rules for <select> as given on my last homework assignment. I used this parser to test them out before submitting the assignment, and never removed them. They're kind of neat, and aren't hurting anything.
 
 TEST CASES
-###########
+######
 
-program int X; begin
-  X = 10;
-  if (X == 5) then
-    X = 7;
-  else
-    X = 10;
-  end;
-  select
-    (X == 10) -> X = X * 2 + 4 * 3;
-  end;
-  write X;
-end
+Compute the factorial of a number:
+  ```
+  program int X , FACT ; begin
+    FACT = 1;
+    read X;
+    while ( X > 0 ) loop
+      write X ;
+      FACT = FACT * X;
+      X = X - 1 ;
+    end ;
+    write FACT , X ;
+  end
+```
+
+Compute the maximum of three values:
+  ```
+  program int A, B, C, MAX; begin
+    read A, B, C;
+    if (A >= B) then
+     if !(A < C) then
+      MAX = A;
+     else
+      MAX = C;
+     end;
+    else
+     if (B <= C) then
+      MAX = C + C;
+     else
+      MAX = B;
+     end;
+    end;
+    write A, B, C, MAX;
+  end
+```
+
+Give the number of identical values:
+  ```
+  program int A, B, C, EQ; begin
+    read A, B, C;
+    if [(A == B) && (B == C)] then
+      EQ = 3;
+    else
+      if [[(A == B) or (B == C)] or (C == A)] then
+        EQ = 2;
+      else
+        EQ = 1;
+      end;
+    end;
+    write A, B, C, EQ;
+  end
+```
 
 
---
+++ interpreter.js
@@ -0,0 +1,366 @@
+CORE = (function(){
+
+var logwin = null;
+function log(message) {
+  if (!logwin) logwin = document.getElementById("conout");
+  if (logwin) logwin.value += message;
+  else alert("Couldn't find log window");
+}
+
+function getStringIn(input, msg) {
+  while (input.length) {
+    var res = input[0];
+    input.splice(0,1);
+    if (res.length > 0)
+      return  { car: res, cdr: input };
+  }
+  return { car: prompt(msg), cdr: [] };
+}
+function getInput(input, msg) {
+  var res = getStringIn(input, msg);
+  if (!res.car.match(/[0-9]+/))
+    throw("Input error: Please input only integers. In an input file, they must be separated by whitespace.");
+  res.car = parseInt(res.car);
+  return res;
+}
+
+function strrep(str, reps) {
+  var res = "";
+  for (var j = 0; j < reps; ++j)
+    res += " ";
+  return res;
+}
+
+/**
+ * This is the default print function. It handles all aspects of pretty-printing a rule.
+ * @param ast    The AST to be printed.
+ * @param ops    The printer operations; these are passed from function to function by the BNF backend.
+ * @param spaces An array of strings to use in place of whitespace tokens from the AST.
+ * @param inds   An array of indentation levels to apply to each rule in the given AST.
+ * @param rinds  An array of booleans denoting whether to respect the current indentation level.
+ */
+function defprint(ast, ops, spaces, inds, rinds) {
+  var res = "", spc = 0, indc = 0, rindc = 0, rind;
+  spaces = (spaces && spaces.length)? spaces : (console.log("ERROR: " + spaces), [' ']);
+  inds = inds || [];
+  rinds = rinds || [];
+  if (ast.type == "rule")
+    for (var i = 0; i < ast.ast.length; ++i)
+      if (ast.ast[i].type == "rule") {
+        var indo = ind;
+        if (indc < inds.length)
+          ind += inds[indc++];
+        if (rindc < rinds.length && (rind = rinds[rindc++]))
+          res += strrep(" ", ind);
+        res += operate(ast.ast[i], ops);
+        ind = indo;
+      }
+      else if (ast.ast[i].type == "token") {
+        if (rindc < rinds.length && (rind = rinds[rindc++]))
+          res += strrep(" ", ind);
+        res += ast.ast[i].name;
+      }
+      else res += spc < spaces.length? spaces[spc++] : spaces[spaces.length - 1];
+  return res;
+}
+
+/**
+ * This is the printer dictionary; it is used by the BNF backend to locate rules for each AST node.
+ * The backend looks up the name of the rule in this dictionary, and then runs the function it finds
+ * on the given AST. These functions will make recursive calls back to operate() as needed.
+ */
+printer = {
+  indents:      ind = 0, ///< This is the amount of indentation used in recurring levels of the pretty-printer; it must be a member of ops so it can be passed through bnf.operate().
+  operate:      operate = bnf.operate,
+  generic:      defprint,
+  "<prog>":     function(ast, ops) { return defprint(ast, ops, [' ', '\n', '\n', ''], [0, 2]); },
+  "<decl seq>": function(ast, ops) { return defprint(ast, ops, ['\n']); },
+  "<stmt seq>": function(ast, ops) { return defprint(ast, ops, ['\n'], [], [1]) + (ast.ind? "\n" : ""); },
+  "<decl>":     function(ast, ops) { return defprint(ast, ops, [' ', '']); },
+  "<id list>":  function(ast, ops) { return defprint(ast, ops, ['', ' ']); },
+  "<stmt>":     defprint, // This is just a big OR of other rules.
+  "<assign>":   function(ast, ops) { return defprint(ast, ops, [' ', ' ', '']); },
+  "<if>":       function(ast, ops) { return defprint(ast, ops, [' ', ' ', '\n', '', '\n', ''], [0, 2, 2], [0, 0, 0, 0, 1, 0, 1]); },
+  "<loop>":     function(ast, ops) { return defprint(ast, ops, [' ', ' ', '\n', ''], [0, 2, 2], [0, 0, 0, 0, 1]); },
+  "<input>":    function(ast, ops) { return defprint(ast, ops, [' ', '']); },
+  "<output>":   function(ast, ops) { return defprint(ast, ops, [' ', '']); },
+  "<cond>":     function(ast, ops) { return defprint(ast, ops, ['', ' ', ' ', '']); },
+  "<comp>":     function(ast, ops) { return defprint(ast, ops, ['', ' ', ' ', '']); },
+  "<exp>":      defprint, "<fac>":      defprint, // For these rules, all spaces are regular spaces.
+  "<op>":       defprint, "<comp op>":  defprint, // These are operators, with no spaces
+  "<id>":       defprint, "<int>":      defprint, // These rules concatenate two other rules without spaces.
+  "<let>":      defprint, "<digit>":    defprint, // These are single characters, with no spaces
+}
+
+
+/* ****************************************************************************************************************** *
+ *  It isn't necessarily the case that the correct grammar was used to build the AST, so these functions help ensure  *
+ * that whatever grammar was used, we can operate on the grammar or report errors accurately otherwise.               *
+ * ****************************************************************************************************************** */
+
+  /// This function is called when a rule is encountered which should have been handled by another rule.
+  function badRule(rule) {
+    throw("Internal error: attempted to perform operation on " + (rule? rule.name? rule.name : "invalid rule" : "undefined rule")
+        + ", which should not happen"); 
+  }
+  /// This function is called to ensure that an AST node is actually a rule.
+  function assertRule(rule) {
+    if (!rule || rule.type != "rule")
+      throw("Internal error: " +
+        (rule? rule.type == "white"? "rule expected, whitespace found"
+           : rule.name? "`" + rule.name + "' should have been a rule" 
+           : "anonymous rule; not a rule" : "undefined rule: not in tree"));
+  }
+  /// This function is called to ensure that an AST node is the token the interpreter thinks it is.
+  function assertToken(ast, ind, name) {
+    if (!ast || ast.type != "rule" || !ast.ast)
+      throw("Internal error: " + (ast? ast.name? ast.name : "anonymous rule" : "undefined rule") + " does not contain expected token");
+    if (!ast.ast[ind] || !ast.ast[ind].name || ast.ast[ind].name != name || ast.ast[ind].type != "token")
+      throw("Internal error: expected token `" + name + "' in rule `" + ast.name + "' was not found");
+  }
+  /// This function is called to ensure that an AST node is whitespace, and so safe to ignore.
+  function assertWhite(ast, ind) {
+    if (!ast || ast.type != "rule" || !ast.ast)
+      throw("Internal error: " + (ast? ast.name? ast.name : "anonymous rule" : "undefined rule") + " does not contain expected token");
+    if (!ast.ast[ind] || ast.ast[ind].type != "white")
+      throw("Internal error: unexpected pattern in rule `" + ast.name + "': white space expected");
+  }
+  /// This function is called on an arbitrary AST node which may not exist; it ensures that the node exists,
+  /// and that it is a rule, and then it invokes the rule's handler method.
+  function operateRule(rule, ops, defined) {
+    if (!defined && !rule) return 0;
+    assertRule(rule);
+    return operate(rule, ops);
+  }
+  /// This function ensures that a result is, in fact, an array of some length.
+  function assertArray(arr, lower, upper, kind) {
+    if (!arr || !arr.length || arr.length < lower || (upper != -1 && arr.length > upper))
+      throw("Internal error: " + kind + " does not have expected length (" + (arr? arr.length : "null") + " should be " +
+        (upper == lower? lower : upper == -1? "at least " + lower : "between " + upper + " and " + lower) + ")");
+    return [].concat(arr);
+  }
+  function nonnull(x, item) {
+    if (x == null)
+      throw("Internal error: " + item + " should not be null");
+    return x;
+  }
+
+/* *********************************************************************** *
+ * Here begins the actual interpreter code.                                *
+ * *********************************************************************** */
+
+/** This maps CORE operator strings to their corresponding JavaScript equivalents. */
+var operators = {
+  ">=": function(x, y) { return x >= y; },   "+":  function(x, y) { return x  + y; },
+  "<=": function(x, y) { return x <= y; },   "-":  function(x, y) { return x  - y; },
+  "==": function(x, y) { return x == y; },   "*":  function(x, y) { return x  * y; },
+  "!=": function(x, y) { return x != y; },   ">":  function(x, y) { return x  > y; },
+  "&&": function(x, y) { return x && y; },   "<":  function(x, y) { return x  < y; },
+  "or": function(x, y) { return x || y; }
+};
+
+function do_operator(op, x, y) {
+  if (op in operators)
+    return operators[op](x, y);
+  throw("Internal error: Unrecognized CORE operator `" + op + "'");
+}
+
+/**
+ * This is the interpreter dictionary; it's similar to the printer one, above, but actually executes the code.
+ */
+interpreter = {
+  symbols:
+    symbolTable = {},
+  symExist:
+    assertSymbolExists = function(sym) {
+      if (!(sym in symbolTable))
+        throw("Program error: Variable `" + sym + "' has not been declared");
+    },
+  input: "", // This allows the caller to inject input
+  generic:
+    function(ast, ops) {
+      if (!ast || ast.name != "<>") // This is our try-all rule that allows any rule in the program.
+        throw("Internal error: Invalid rule to interpret: " + (ast? ast.name : "(no rule passed)") + ": Rule not found");
+      assertArray(ast.ast, 1, 1, "AST");
+      if (ast.ast[0].name != "<prog>")
+        throw("Mechanics error: A CORE program must begin with `program'");
+      symbolTable = {};
+      ops.input = ops.input.split(/[\s\n]+/);
+      console.log(ops.input);
+      return operate(ast.ast[0], ops);
+    },
+  evalSeq: // Function to evaluate every rule in an AST, sequentially.
+    evalSeq = function(ast, ops) {
+      var res = null;
+      for (var i = 0; i < ast.ast.length; ++i)
+        if (ast.ast[i].type == "rule")
+          res = operate(ast.ast[i], ops);
+      return res;
+    },
+  
+  // These four rules evaluate their children sequentially; there's nothing special to do for them.
+  "<prog>":     evalSeq,
+  "<decl seq>": evalSeq,
+  "<stmt seq>": evalSeq,
+  "<stmt>":     evalSeq,
+  
+  // These rules are for working with variable names
+  "<decl>": // Declares variables, adding them to our symbol table.
+    function(ast, ops) {
+      assertToken(ast, 0, "int");
+      assertWhite(ast, 1);
+      var ids = operateRule(ast.ast[2], ops, true);
+      if (!ids || !ids.length || ids.length < 1)
+        throw("Internal error: empty ID list?");
+      for (var i = 0; i < ids.length; ++i)
+        symbolTable[ids[i]] = null; // Declare, but do not initialize
+    },
+  "<id list>": // Returns an array of IDs.
+    function(ast, ops) {
+      var res = assertArray(operateRule(ast.ast[0], printer, true), 1, -1, "ID list");
+      if (ast.ast.length > 1) {
+        assertWhite(ast, 1); assertWhite(ast, 3);
+        assertToken(ast, 2, ',');
+        var ida = operateRule(ast.ast[4], ops, true);
+        var rest = assertArray(ida, 1, -1, "ID list");
+        for (var i = 0; i < rest.length; ++i)
+          res.push(rest[i]);
+      }
+      return res;
+    },
+  "<assign>":
+    function(ast, ops) {
+      var id = operateRule(ast.ast[0], printer, true); // This just asks the printer to print the variable name. Terrible, eh?
+      assertSymbolExists(id); // Make sure it's in our symbol table
+      assertWhite(ast, 1);
+      assertToken(ast, 2, "=");
+      assertWhite(ast, 3);
+      var val = operateRule(ast.ast[4], ops, true);
+      return symbolTable[id] = val;
+    },
+  
+  // These are the rules moving actual code.
+  "<if>":
+    function(ast, ops) {
+      assertToken(ast, 0, "if");
+      assertWhite(ast, 1);
+      var cond = operateRule(ast.ast[2], ops, true);
+      assertWhite(ast, 3);
+      assertToken(ast, 4, "then");
+      assertWhite(ast, 5);
+      if (cond)
+        return operateRule(ast.ast[6], ops, true);
+      assertWhite(ast, 7);
+      // Token 8 is else, 9 is white, and 10 is the else code, if ind = 1
+      if (ast.ind)
+        return operateRule(ast.ast[10], ops, false); // The else code is optional.
+    },
+  "<loop>":
+    function(ast, ops) {
+      assertToken(ast, 0, "while");
+      assertWhite(ast, 1);
+      assertWhite(ast, 3);
+      assertToken(ast, 4, "loop");
+      assertWhite(ast, 5);
+      assertWhite(ast, 7);
+      assertToken(ast, 8, "end");
+      var res = 0;
+      while (operateRule(ast.ast[2], ops, true))
+        res = operateRule(ast.ast[6], ops, true);
+      return res;
+    },
+  "<input>":
+    function(ast, ops) {
+      assertToken(ast, 0, "read"); assertWhite(ast, 1);
+      var vars = assertArray(operateRule(ast.ast[2], ops, true), 1, -1, "ID list");
+      for (var i = 0; i < vars.length; ++i) {
+        var val = getInput(ops.input, "Please input " + vars[i] + ":");
+        symbolTable[vars[i]] = val.car;
+      }
+    },
+  "<output>":
+    function(ast, ops) {
+      assertToken(ast, 0, "write"); assertWhite(ast, 1);
+      var vars = assertArray(operateRule(ast.ast[2], ops, true), 1, -1, "ID list");
+      var maxlen = 0, msg = "";
+      for (var i = 0; i < vars.length; ++i)
+        maxlen = Math.max(maxlen, vars[i].length);
+      for (var i = 0; i < vars.length; ++i)
+        msg += strrep(" ", maxlen - vars[i].length) + vars[i] + " = " + symbolTable[vars[i]] + "\n";
+      log(msg);
+      return 0;
+    },
+  "<cond>": 
+    function(ast, ops) {
+      switch (ast.ind) {
+        case 0: // <comp>
+          return operateRule(ast.ast[0], ops, true);
+        case 1: // !<cond>
+          assertToken(ast, 0, "!");
+          assertWhite(ast, 1);
+          return !operateRule(ast.ast[2], ops, true);
+        case 2: // [<cond> && <cond>]
+          assertToken(ast, 0, "["); assertWhite(ast, 1); assertWhite(ast, 3); assertToken(ast, 4, "&&"); assertWhite(ast, 5);
+          return operateRule(ast.ast[2], ops, true) && operateRule(ast.ast[6], ops, true);
+        case 3: // [<cond> or <cond>]
+          assertToken(ast, 0, "["); assertWhite(ast, 1); assertWhite(ast, 3); assertToken(ast, 4, "or"); assertWhite(ast, 5);
+          return operateRule(ast.ast[2], ops, true) || operateRule(ast.ast[6], ops, true);
+      }
+      throw("Internal error: Condition does not have expected format");
+    },
+  "<comp>":
+    function(ast, ops) {
+      assertToken(ast, 0, "(");
+      assertWhite(ast, 1); assertWhite(ast, 3);
+      assertWhite(ast, 5); assertWhite(ast, 7);
+      assertToken(ast, 8, ")");
+      return do_operator(operateRule(ast.ast[4], ops, true), operateRule(ast.ast[2], ops, true), operateRule(ast.ast[6], ops, true));
+    },
+  "<exp>":
+    function(ast, ops) {
+      if (ast.ast.length == 1)
+        return operateRule(ast.ast[0], ops, true);
+      assertWhite(ast, 1); assertWhite(ast, 3);
+      return do_operator(ast.ast[2].name, operateRule(ast.ast[0], ops, true), operateRule(ast.ast[4], ops, true));
+    },
+  "<fac>":
+    function(ast, ops) {
+      if (ast.ast.length == 1)
+        return operateRule(ast.ast[0], ops, true);
+      assertWhite(ast, 1); assertWhite(ast, 3);
+      return do_operator(ast.ast[2].name, operateRule(ast.ast[0], ops, true), operateRule(ast.ast[4], ops, true));
+    },
+  "<op>":
+    function(ast, ops) {
+      if (ast.ast.length == 1)
+        return operateRule(ast.ast[0], ops, true);
+      assertToken(ast, 0, "(");
+      assertWhite(ast, 1);
+      assertWhite(ast, 3);
+      assertToken(ast, 4, ")");
+      
+    },
+  "<id>":
+    function(ast, ops) {
+      var id = operate(ast, printer);
+      assertSymbolExists(id);
+      var res = symbolTable[id];
+      if (res == null)
+        throw("Program error: Attempting to use a variable that has been declared, but not assigned");
+      return res;
+    },
+  "<int>":
+    function(ast, ops) {
+      var num = operate(ast, printer);
+      if (!num.match(/[0-9]+/))
+        throw("Internal error: number is not numeric");
+      if (num == "0") return 0;
+      return parseInt(num, 10);
+    },
+  "<comp op>":  function(ast, ops) { assertArray(ast.ast, 1, 1, "AST"); return nonnull(ast.ast[0].name); },
+  "<let>":      badRule,
+  "<digit>":    badRule
+};
+
+return { interpreter: interpreter, printer: printer };
+})();
